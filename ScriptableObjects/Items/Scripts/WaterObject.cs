c92b2cd8775e0e0a4c2210b88b2c5a6903a48fad
imanmoreira <moreira.i@husky.neu.edu>
2022-03-28 14:04:37 -0400

    Updating water scriptable object

--- ScriptableObjects/Items/Scripts/WaterObject.cs
+++ ScriptableObjects/Items/Scripts/WaterObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item Object", menuName = "Inventory System/Items/Water")]
+[CreateAssetMenu(fileName = "New Water Object", menuName = "Inventory System/Items/Water")]
 public class WaterObject : ItemObject
 {
     public void Awake()

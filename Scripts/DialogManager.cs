d4ba1b79e171b1664a5107963e0f6afe726a72bb
cynthiaxie62 <cynthiaxie62@outlook.com>
2022-04-29 21:48:52 -0400

    fix instructions

--- Scripts/DialogManager.cs
+++ Scripts/DialogManager.cs
@@ -22,7 +22,6 @@ public class DialogManager : MonoBehaviour
         sentences.Clear();
         sentences = new Queue<string>();
         sentences.Enqueue("Hello youngster! I see you finally woke up...");
-
         sentences.Enqueue("I was just going for my morning stroll when I saw you lying on the ground. ");
         sentences.Enqueue("I'll tell you this since you look like you aren't from around here: ");
         sentences.Enqueue("Beware of the Crittercrops! They will attack you and kill you if you get too close.");
@@ -30,8 +29,9 @@ public class DialogManager : MonoBehaviour
         sentences.Enqueue("To damage CritterCrops, aim your spray bottle to stray pesticide on them. That should give them damage.");
         sentences.Enqueue("If you manage to kill them, you can pick up the seeds they drop by making contact.");
         sentences.Enqueue("You can grow crops out of these seeds for money.");
-        sentences.Enqueue("Then you can try planting in this farm land here by pressing q.");
-        sentences.Enqueue("Once the plant's ready, press 3 to use the shovel to harvest it.");
+        sentences.Enqueue("Try planting in this farm land here by pressing q.");
+        sentences.Enqueue("Collect water at the well and press 2 to use the watering can on your crops.");
+        sentences.Enqueue("Once the plant's fully grown, press 3 to use the shovel to harvest it.");
         sentences.Enqueue("To open and close the inventory, press x.");
         sentences.Enqueue("To run, press Shift.");
         sentences.Enqueue("If you forget anything, press h for help or come talk to me again!");
